ea57d2a096117e7e820f2bf2a975ecebf9904ef0
Jax <jaxlombard@gmail.com>
2020-07-23 17:31:35 -0500

    added a file that finds the zip code of the address

--- zipCode.py
+++ zipCode.py
@@ -1,8 +1,24 @@
 import googlemaps
 from datetime import datetime
 from mapit import address
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
-gmaps = googlemaps.Client(key='')
-geocode_result = gmaps.geocode(address)
-zipcode = details['Locality']['PostalCode']['PostCodeNumber']
-print(zipcode)
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.implicitly_wait(5)
+driver.get('https://www.unitedstateszipcodes.org/')
+
+def searchZip():
+    global zipCode
+    search_box = driver.find_element_by_css_selector('#q')
+    search_box.send_keys(address)
+    driver.find_element_by_css_selector('#search-forms > div.col-xs-12.col-lg-7 > div > span.input-group-btn > button > i').click()
+    for char in driver.find_elements_by_xpath('//*[@id="map-info"]/table/tbody/tr[4]/td'):
+        zipCode = ((char.text)[:5])
+    print("The zipcode is " + zipCode)
+
+searchZip()
+
+
+driver.implicitly_wait(5)
+driver.quit()
